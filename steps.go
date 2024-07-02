2c9e7392f7e7c2201ec4d4b4b929ff61046f7c09
sara-asadi <sara.asadi22@gmail.com>
2024-07-02 10:40:19 +0330

    with DP

--- steps.go
+++ steps.go
@@ -3,21 +3,37 @@ package main
 import "fmt"
 
 func main() {
-
-	// steps := os.Args[1]
-	calculateSteps(5, "")
-
+	calculateSteps(4)
+	fmt.Println(paths[4])
 }
 
-func calculateSteps(stepNum int, steps string) {
+var paths map[int][]string = make(map[int][]string)
+
+func calculateSteps(stepNum int) {
+
 	if stepNum == 0 {
-		fmt.Println(steps)
+		paths[0] = []string{""}
 		return
 	}
+
 	if stepNum >= 2 {
-		calculateSteps(stepNum-2, steps + "2 ")
+		if _, ok := paths[stepNum-2]; !ok {
+			calculateSteps(stepNum - 2)
+		}
+
+		for _, previousPath := range paths[stepNum-2] {
+			paths[stepNum] = append(paths[stepNum], "2 "+previousPath)
+		}
 	}
+
 	if stepNum >= 1 {
-		calculateSteps(stepNum-1, steps + "1 ")
+		if _, ok := paths[stepNum-1]; !ok {
+			calculateSteps(stepNum - 1)
+		}
+
+		for _, previousPath := range paths[stepNum-1] {
+			paths[stepNum] = append(paths[stepNum], "1 "+previousPath)
+		}
 	}
+
 }
